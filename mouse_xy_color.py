9fc1e4e4d3d4aaaad7a59f9f43da2bdbaba323ab
Surachai Rodsai <surachairobotic@gmail.com>
2023-02-03 11:25:53 +0700

    every 5 in

--- mouse_xy_color.py
+++ mouse_xy_color.py
@@ -3,7 +3,7 @@ import keyboard
 
 while True:
     x, y = pyautogui.position()
-    color = pyautogui.screenshot().getpixel((960, 138))
+    color = pyautogui.screenshot().getpixel((x, y))
     print(f'x: {x}, y: {y}, color: {color}')
     if keyboard.is_pressed('q'):
         break
